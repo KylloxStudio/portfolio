e9407dde04321efa68196c3f4b941f0f37e2e708
Kyllox <kyllox4804@gmail.com>
2022-07-31 12:55:43 +0900

    Update

--- pages/contact.js
+++ pages/contact.js
@@ -64,59 +64,73 @@ export default function Contact() {
 
   const sendEmail = (e) => {
     e.preventDefault();
+    let pass = [false, false, false, false, false];
     select('.error-label', true).forEach((label) => {
       label.innerHTML = "";
     });
     document.getElementsByName('title').forEach((input) => {
       if (input.value == null || input.value == "") {
+        pass[0] = false;
         select('.error-label', true)[0].innerHTML = "<label class='error' for='#'>Please Enter a Title.</label>";
-        return;
+      } else {
+        pass[0] = true;
       }
     });
     document.getElementsByName('name').forEach((input) => {
       if (input.value == null || input.value == "") {
+        pass[1] = false;
         select('.error-label', true)[1].innerHTML = "<label class='error' for='#'>Please Enter Your Name.</label>";
-        return;
+      } else {
+        pass[1] = true;
       }
     });
     document.getElementsByName('email').forEach((input) => {
       if (input.value == null || input.value == "") {
+        pass[2] = false;
         select('.error-label', true)[2].innerHTML = "<label class='error' for='#'>Please Enter Your Email.</label>";
-        return;
       }
       let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
       if (!regEmail.test(input.value)) {
+        pass[2] = false;
         select('.error-label', true)[2].innerHTML = "<label class='error' for='#'>Please Enter a Valid Email.</label>";
-        return;
+      } else {
+        pass[2] = true;
       }
     });
     document.getElementsByName('message').forEach((input) => {
       if (input.value == null || input.value == "") {
+        pass[3] = false;
         select('.error-label', true)[3].innerHTML = "<label class='error' for='#'>Please Enter the Messages.</label>";
-        return;
+      } else {
+        pass[3] = true;
       }
     });
     if (select('#g-recaptcha-response').value == null || select('#g-recaptcha-response').value == "") {
+      pass[4] = false;
       select('.error-label', true)[4].innerHTML = "<label class='error' for='#'>Please Proceed With reCAPTCHA Certification.</label>";
-      return;
+    } else {
+      pass[4] = true;
     }
-    select('.error-label', true).forEach((label) => {
-      label.remove();
-    });
-    let loader = select('#submit-loader');
-    fadeIn(loader);
-    emailjs.sendForm('service_kyllox', 'template_kyllox', form.current, 'user_ObDVdiMQlk2RAhavouTNx').then((response) => {
-      fadeOut(loader);
-      hide(select('#message-warning'));
-      fadeOut(select('#contact-form'));
-      select('#message-success').innerHTML = "<i className='fa fa-check'></i>The message was sent successfully. The answer will be sent to the email address you filled out.</p><br>" + "<p>" + response.status + "</p>" + " " + response.text;
-      fadeIn(select('#message-success'));
-      hide(select('#message-warning'));
-    }, function (e) {
-      fadeOut(loader);
-      select('#message-warning').innerHTML = "<i className='fa fa-xmark'></i>Failed to send message. Please try again in a few minutes.<br>" + "<p>" + e.status + "</p>" + " " + e.text;
-      fadeIn(select('#message-warning'));
-    });
+    if (pass[0] && pass[1] && pass[2] && pass[3] && pass[4]) {
+      select('.error-label', true).forEach((label) => {
+        label.remove();
+      });
+      let loader = select('#submit-loader');
+      fadeIn(loader);
+      emailjs.sendForm('service_kyllox', 'template_kyllox', form.current, 'user_ObDVdiMQlk2RAhavouTNx').then((response) => {
+        fadeOut(loader);
+        hide(select('#message-warning'));
+        fadeOut(select('#contact-form'));
+        select('#message-success').innerHTML = "<i className='fa fa-check'></i>The message was sent successfully. The answer will be sent to the email address you filled out.</p><br>" + "<p>" + response.status + "</p>" + " " + response.text;
+        fadeIn(select('#message-success'));
+        hide(select('#message-warning'));
+      }, function (e) {
+        fadeOut(loader);
+        fadeOut(select('#contact-form'));
+        select('#message-warning').innerHTML = "<i className='fa fa-xmark'></i>Failed to send message. Please try again in a few minutes.<br>" + "<p>" + e.status + "</p>" + " " + e.text;
+        fadeIn(select('#message-warning'));
+      });
+    }
   };
   return (
     <Layout>
